49f098becfc939df0b3bfc44adb461f405151f0c
Elvis Estevan <elvis.estevan65@gmail.com>
2024-11-13 15:49:53 -0300

    exploring variables

---
+++ variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+
+	total := 0.1
+
+	total2 := 0.1
+
+	total3 := 0.1
+
+	fmt.Println(total + total2 + total3)
+
+}
